8524980536ff1c3bd3aca3a54ddd05f58c267b4e
thomasnappee <thomas.napro@gmail.com>
2024-02-28 18:30:58 +0100

    implem prompt manager

--- raglab/app.py
+++ raglab/app.py
@@ -1,13 +1,130 @@
+from sqlalchemy import create_engine, Table, MetaData, delete, select
+from typing import Mapping, Sized, Iterable
+from chromadb import Client, Settings, HttpClient
+
+from langchain_community.llms.openai import OpenAIChat
+from langchain_community.llms.gpt4all import GPT4All
+from langchain_community.embeddings.gpt4all import GPT4AllEmbeddings
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_community.document_loaders import NotionDirectoryLoader
+from langchain.prompts import ChatPromptTemplate
+
+import json
+
+import config2py
+
+POSTGRESS_TEST_DB_URL = config2py.config_getter('POSTGRESS_TEST_DB_URL')
+
+class PostgresTableRows(Sized, Iterable):
+    def __init__(self, engine, table_name):
+        self.engine = engine
+        self.table_name = table_name
+        self.metadata = MetaData()
+        self.table = Table(self.table_name, self.metadata, autoload_with=self.engine)
+
+    def __iter__(self):
+        query = select(self.table)
+        with self.engine.connect() as connection:
+            result = connection.execute(query)
+            for row in result:
+                yield row
+
+    def __len__(self):
+        query = select(self.table)
+        with self.engine.connect() as connection:
+            result = connection.execute(query)
+            return result.rowcount
+        
+class PostgresBaseColumnsReader(Mapping):
+    """Here, keys are column names and values are column values"""
+    def __init__(self, engine, table_name):
+        self.engine = engine
+        self.table_name = table_name
+        self.metadata = MetaData()
+        self.table = Table(self.table_name, self.metadata, autoload_with=self.engine)
+        
+    def __iter__(self):
+        return (column_obj.name for column_obj in self.table.columns)
+    
+    def __len__(self):
+        return len(self.table.columns)
+    
+    def __getitem__(self, key):
+        # TODO: Finish
+        query = select(self.table).with_only_columns([self.table.c[key]])
+        with self.engine.connect() as connection:
+            result = connection.execute(query)
+            return result.fetchall()
+    
+
+from typing import Callable  
+
+
+class PostgresBaseKvReader(Mapping):
+    """A mapping view of a table, 
+    where keys are values from a key column and values are values from a value column.
+    There's also a filter function that can be used to filter the rows.
+    """
+    def __init__(
+            self, engine, table_name, 
+            key_columns=None, 
+            value_columns=None,
+            filt=None
+        ):
+        self.engine = engine
+        self.table_name = table_name
+        self.metadata = MetaData()
+        self.table = Table(self.table_name, self.metadata, autoload_with=self.engine)
+        self.filt = filt
+
+    def __iter__(self):
+        query = select(self.table)
+        with self.engine.connect() as connection:
+            result = connection.execute(query)
+            for row in result:
+                yield row
+
+    def __len__(self):
+        query = select(self.table)
+        with self.engine.connect() as connection:
+            result = connection.execute(query)
+            return result.rowcount
+        
+    def __getitem__(self, key):
+        query = select(self.table)
+        with self.engine.connect() as connection:
+            result = connection.execute(query)
+            return result.fetchall()
+        
+        
+class PostgressTables(Mapping):
+    def __init__(self, engine):
+        self.engine = engine
+        self.metadata = MetaData()
+        self.metadata.reflect(bind=self.engine)
+
+    def __getitem__(self, key):
+        return PostgresBaseKvReader(self.engine, key)
+        # Or do something with this:
+        # return self.metadata.tables[key]
+
+    def __iter__(self):
+        return iter(self.metadata.tables)
+
+    def __len__(self):
+        return len(self.metadata.tables)
+
+
 import os
 import re
 from fastapi import Depends, FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
-from dotenv import load_dotenv
 import uvicorn
 
-from raglab2 import RaglabSessionBuilder, RaglabSession
+from raglab2 import Raglab2, RaglabSessionBuilder, RaglabSession
 
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
@@ -58,9 +175,8 @@ def get_db():
     finally:
         db.close()
 
-load_dotenv()
 app = FastAPI()
-app.secret_key =  os.getenv("RAGLAB_API_KEY")
+app.secret_key =  config2py.config_getter("RAGLAB_API_KEY")
 origins = ["*"]
 app.add_middleware(CORSMiddleware,
     allow_origins=origins,
@@ -69,6 +185,8 @@ app.add_middleware(CORSMiddleware,
     allow_headers=["*"])
 
 auth_layer = APIKeyHeader(name="API_TOKEN")
+
+
 
 @app.get("/app_list")
 def app_list(token:str=Depends(auth_layer), db:Session = Depends(get_db)) :  
@@ -80,73 +198,103 @@ def app_list(token:str=Depends(auth_layer), db:Session = Depends(get_db)) :
 
 @app.get("/prompt")
 def get_template_list(token:str=Depends(auth_layer), db:Session = Depends(get_db)) :
-
     result = db.query(PromptTemplate).all()
 
     lst = []
 
     for r in result :
         lst.append({
-            "id" : r.id,
             "name" : r.name,
-            "content" : r.template
+            "template" : r.template
         })
 
     return lst
 
 @app.get("/prompt/editor")
-def get_editor(id:str, db:Session=Depends(get_db)) :
-    result = db.query(PromptTemplate).filter(PromptTemplate.id == id).first()
+def get_editor(name:str, token:str=Depends(auth_layer), db:Session=Depends(get_db)) :
+    result = db.query(PromptTemplate).filter(PromptTemplate.name == name).first()
 
     if result is None :
         return Response(status_code=404)
 
+    print
+
     return {
         "prompt" : {
-            "id": result.id,
             "name" : result.name,
             "template": result.template
         },
-        "rjsf_ui" : result.rjsf_ui
+        "rjsf_ui" : json.loads(result.rjsf_ui)
     }
 
 class SavePromptTemplateRequest(BaseModel) :
-    id: int
     name: str
     template: str
 
 def get_rjsf_from_template(template:str) :
     parameters = parse_parameters(template)
-    schema = """
+    json_schema = """
 {
     'title': 'Prompt arguments', 
     'type': 'object', 
-    'properties':
-""" + ",".join("{'type': 'string', 'title': '{p}'}" for p in parameters) + "} }"
-    return schema
+    'properties':{
+""" + ",".join(f"\'{p}\' : {{'type': 'string', 'title': '{p}'}}" for p in parameters) + "}}"
+    
+    #ui_schema ="{"+",".join([f"\"{p}\" : {{\"ui:widget\": \"textarea\"}}" for p in parameters])+"}"
+
+    return json_schema
+
+
 
 @app.post("/prompt/editor")
-def save_prompt(request:SavePromptTemplateRequest, db:Session=Depends(get_db)) :    
-    prompt_template = db.query(PromptTemplate).filter(PromptTemplate.id == request.id).first()
+def save_prompt(request:SavePromptTemplateRequest, token:str=Depends(auth_layer), db:Session=Depends(get_db)) :    
+    print(f"User {token} wants to save {request.name}")
+
+    prompt_template = db.query(PromptTemplate).filter(PromptTemplate.name == request.name).first()
 
     if prompt_template is None :
         prompt_template = PromptTemplate()
 
     prompt_template.name = request.name
     prompt_template.template = request.template
-    prompt_template.rjsf_ui = get_rjsf_from_template(request.template)
+
+
+    dico = {}
+
+    for p in parse_parameters(request.template) : 
+        dico[p] = {'type': 'string', 'title': f'{p}'}
+
+    prompt_template.rjsf_ui = json.dumps({
+            'title': 'Prompt arguments', 
+            'type': 'object', 
+            'properties': dico
+        })
+
     db.add(prompt_template)
     db.commit()
+    
+    return {
+        "prompt" : {
+            "name" : prompt_template.name,
+            "template": prompt_template.template
+        },
+        "rjsf_ui" : {
+            'title': 'Prompt arguments', 
+            'type': 'object', 
+            'properties': dico
+        }
+    }
+
+@app.delete("/prompt/editor")
+def delete_prompt_template(keys:list[str], token:str=Depends(auth_layer), db:Session=Depends(get_db)) :
+
+    print(f"User {token} wants to delete {keys}")
+
+    for key in keys :
+        db.execute(delete(PromptTemplate).where(PromptTemplate.name == key))
+        print(f"Deleted {key}")
+    db.commit()
     return Response(status_code=200)
-
-
-from chromadb import Client, Settings
-
-from langchain_community.llms.openai import OpenAIChat
-from langchain_community.llms.gpt4all import GPT4All
-from langchain_community.embeddings.gpt4all import GPT4AllEmbeddings
-from langchain_community.vectorstores.chroma import Chroma
-from langchain.prompts import ChatPromptTemplate
 
 class AskPromptRequest(BaseModel) :
     template:str
@@ -156,27 +304,39 @@ def parse_parameters(template:str) -> list[str] :
     pattern = r'\{([^}]*)\}'
     return re.findall(pattern, template)
 
+import oa
+from oa import prompt_function
+
+def _default_chat():
+    from oa import chat
+
+    return chat
+
+from oa import prompt_function
+from i2 import Sig
+
+def prompt_execution_adapter(prompt_template, params):
+    f = prompt_function(prompt_template)
+    sig = Sig(f)
+    kwargs = sig.map_arguments(params, ignore_kind=True)
+    return f(**kwargs)
+
 @app.post("/prompt/editor/ask")
 def ask_prompt(request:AskPromptRequest, token:str=Depends(auth_layer)) :
-    model = OpenAIChat(model_name="gpt-3.5-turbo")
+    return {
+        "answer": prompt_execution_adapter(request.template, request.parameters)
+    }
 
-    parameters = parse_parameters(request.template)
-    i = 0
-    for p in parameters :        
-        request.template = request.template.replace("{"+p+"}", request.parameters[i])
-        i += 1
-
-    return model.ask(request.template)
 
 @app.get("/app/{name}/")
 def run_app(name:str, token:str=Depends(auth_layer), db:Session = Depends(get_db)) :
     result = db.query(App).filter(App.name == name).first()
 
     #### TODO: Build from App ####
-
-    client = Client(host='149.202.47.109', port="45000", settings=Settings(chroma_client_auth_provider="chromadb.auth.token.TokenAuthClientProvider", chroma_client_auth_credentials=os.getenv("CHROMA_KEY")))
+    chroma_key = config2py.config_getter("CHROMA_KEY")
+    client = HttpClient(host='149.202.47.109', port="45000", settings=Settings(chroma_client_auth_provider="chromadb.auth.token.TokenAuthClientProvider", chroma_client_auth_credentials=chroma_key))
     embedding = GPT4AllEmbeddings(client=client)
-    db = Chroma(collection_name="name", client=client, embedding_function=embedding)
+    db = Chroma(collection_name="poc", client=client, embedding_function=embedding)
     llm = GPT4All("all-MiniLM-L6-v2-f16.gguf")
     prompt_template = """Answer the question based only on the following context:
     {context}
--- raglab/mermaid/prompt_editor.md
+++ raglab/mermaid/prompt_editor.md
@@ -3,11 +3,11 @@
         participant Frontend
         participant Backend
         Frontend->>Backend: GET /prompt
-        Backend-->>Frontend: JSON { [TEMPLATE_NAME] } (liste des templates)
+        Backend-->>Frontend: JSON { [{name:str}] } (liste des templates)
         Frontend->>Backend: GET /prompt/editor?name (récupérer le template + les forms)
-        Backend-->>Frontend: JSON {template, rjsf_ui}  (template + ui)
+        Backend-->>Frontend: JSON {template:str, rjsf_ui:str}  (template + ui)
         Frontend->>Backend: POST /prompt/editor JSON (poster le template pour le sauvegarder)
         Backend-->>Frontend: JSON {template, rjsf_ui} (template + rjsf_ui mis à jour)
         Frontend->>Backend: GET /prompt/editor/ask?query
-        Backend-->>Frontend: JSON { answer }
+        Backend-->>Frontend: JSON { answer:str }
 ```
