7308f94688cb780b9a0cd61e698ba6997fd7ee39
thomasnappee <thomas.napro@gmail.com>
2024-03-10 15:56:47 +0100

    migrate to sqldol

--- raglab/app.py
+++ raglab/app.py
@@ -1,31 +1,29 @@
-
-import os
-import re
+import config2py
+from http.client import HTTPException
 from fastapi import Depends, FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader
-from pydantic import BaseModel
-import uvicorn
-
-from raglab2 import Raglab2, RaglabSessionBuilder, RaglabSession
-
-from sqlalchemy import create_engine, Table, MetaData, delete, select
-from sqlalchemy.orm import declarative_base, sessionmaker, Session
-
-from sqldol.stores import SqlDictsReader
-
-from typing import Mapping, Sized, Iterable
-
+from i2 import Sig
 import json
+from pydantic import BaseModel
+import oa
+from oa import chat, prompt_function
+import re
+from sqlalchemy import create_engine, delete
+from sqlalchemy.orm import sessionmaker, Session
 
-import config2py
+from sqldol.stores import SqlDictReader, SqlDictStore
+import uvicorn
+from dotenv import load_dotenv
 
-DATABASE_URL = config2py.config_getter('DATABASE_URL')
+load_dotenv()
+
+
+DATABASE_URL = config2py.config_getter("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-running_sessions = dict[str, RaglabSession]
 
 def get_db():
     db = SessionLocal()
@@ -34,170 +32,192 @@ def get_db():
     finally:
         db.close()
 
+
 app = FastAPI()
-app.secret_key =  config2py.config_getter("RAGLAB_API_KEY")
+app.secret_key = config2py.config_getter("RAGLAB_API_KEY")
 origins = ["http://localhost:3000", "https://raglab-ui.vercel.app"]
-app.add_middleware(CORSMiddleware,
+app.add_middleware(
+    CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"])
+    allow_headers=["*"],
+)
+
+cache_token_user_id = {}
 
 auth_layer = APIKeyHeader(name="API_TOKEN")
 
-def get_user_id_by_token(token) :
-    user = SqlDictsReader(engine=engine, table_name="users", key_columns="token", value_columns=["id"])
-    user_id = user[token]
-    return user_id
+
+def check_auth_layer(token: str = Depends(auth_layer)):
+    if token in cache_token_user_id:
+        return True
+
+    raise HTTPException(
+        status_code=401,
+        detail="Invalid API Key",
+    )
+
+
+def get_auth_id_layer(token: str = Depends(auth_layer)):
+    user = SqlDictReader(
+        engine=engine, table_name="users", key_columns="token", value_columns=["id"]
+    )
+    user_id = user[token]["id"]
+    cache_token_user_id[token] = user_id
+
+    return int(user_id)
+
 
 @app.get("/app_list")
-def app_list(token:str=Depends(auth_layer), db:Session = Depends(get_db)) :  
-    user_id = get_user_id_by_token(token)
-    
-    permissions = SqlDictsReader(
-        engine=engine, 
-        table_name="app_permission", 
-        key_columns="id", 
-        value_columns=["app_id", "user_id"])
-    
+def app_list(user_id: str = Depends(get_auth_id_layer)):
+
+    permissions = SqlDictReader(
+        engine=engine,
+        table_name="app_permission",
+        key_columns="id",
+        value_columns=["app_id", "user_id"],
+    )
+
     permissions_id = []
 
-    for id in permissions :
+    for id in permissions:
         if permissions[id]["user_id"] == user_id:
             permissions_id.append(permissions[id]["app_id"])
 
-    apps = SqlDictsReader(engine=engine, table_name="app", key_columns="id", value_columns=["name"])
-    app_names = [app[id]["name"] for id in apps]
-    return {
-        "names" : app_names
-    }
+    apps = SqlDictReader(
+        engine=engine, table_name="app", key_columns="id", value_columns=["name"]
+    )
+
+    app_names = [apps[id]["name"] for id in apps]
+
+    return {"names": app_names}
+
 
 @app.get("/prompt")
-def get_template_list(token:str=Depends(auth_layer), db:Session = Depends(get_db)) :
-    user_id = get_user_id_by_token(token)
-    
-    store = SqlDictsReader(
-        engine=engine, 
-        table_name="prompt_template", 
-        key_columns="id", 
-        value_columns=["name", "template", "owner_id"])
+def get_template_list(user_id=Depends(get_auth_id_layer)):
+    print(f"User {user_id} wants to get prompt templates")
+
+    store = SqlDictReader(
+        engine=engine,
+        table_name="prompt_template",
+        key_columns="id",
+        value_columns=["name", "template", "owner_id"],
+    )
 
     templates = []
-    for key in store :
-        if store[key]["owner_id"] == user_id :
-            templates.add(store[key])
+    for key in store:
+        if store[key]["owner_id"] == user_id:
+            templates.append(store[key])
 
-    lst = [{"name": t["name"], "template": t["name"]} for t in templates]
+    lst = [{"name": t["name"], "template": t["template"]} for t in templates]
     return lst
 
-@app.get("/prompt/editor")
-def get_editor(name:str, token:str=Depends(auth_layer), db:Session=Depends(get_db)) :
-    store = SqlDictsReader(
-        engine=engine, 
-        table_name="prompt_template", 
-        key_columns="name", 
-        value_columns=["name"])
 
-    if result is None :
+@app.get("/prompt/editor")
+def get_editor(name: str):
+    store = SqlDictReader(
+        engine=engine,
+        table_name="prompt_template",
+        key_columns="name",
+        value_columns=["name", "template", "rjsf_ui"],
+    )
+
+    try:
+        result = store[name]
+    except:
         return Response(status_code=404)
 
-    print
-
     return {
-        "prompt" : {
-            "name" : result.name,
-            "template": result.template
-        },
-        "rjsf_ui" : json.loads(result.rjsf_ui)
+        "prompt": {"name": result["name"], "template": result["template"]},
+        "rjsf_ui": json.loads(result["rjsf_ui"]),
     }
 
-class SavePromptTemplateRequest(BaseModel) :
+
+class SavePromptTemplateRequest(BaseModel):
     name: str
     template: str
 
-def get_rjsf_from_template(template:str) :
-    parameters = parse_parameters(template)
-    json_schema = """
-{
-    'title': 'Prompt arguments', 
-    'type': 'object', 
-    'properties':{
-""" + ",".join(f"\'{p}\' : {{'type': 'string', 'title': '{p}'}}" for p in parameters) + "}}"
-    
-    #ui_schema ="{"+",".join([f"\"{p}\" : {{\"ui:widget\": \"textarea\"}}" for p in parameters])+"}"
-
-    return json_schema
-
-
 
 @app.post("/prompt/editor")
-def save_prompt(request:SavePromptTemplateRequest, token:str=Depends(auth_layer), db:Session=Depends(get_db)) :    
-    print(f"User {token} wants to save {request.name}")
+def save_prompt(request: SavePromptTemplateRequest, user_id=Depends(get_auth_id_layer)):
+    print(f"User {user_id} wants to save {request.name}")
 
-    prompt_template = db.query(PromptTemplate).filter(PromptTemplate.name == request.name).first()
-
-    if prompt_template is None :
-        prompt_template = PromptTemplate()
-
-    prompt_template.name = request.name
-    prompt_template.template = request.template
-    
+    store = SqlDictStore(
+        engine=engine,
+        table_name="prompt_template",
+        key_columns="name",
+        value_columns=["template", "rjsf_ui", "owner_id"],
+    )
 
     dico = {}
+    for p in parse_parameters(request.template):
+        dico[p] = {"type": "string", "title": p}
 
-    for p in parse_parameters(request.template) : 
-        dico[p] = {'type': 'string', 'title': p}
-
-    prompt_template.rjsf_ui = json.dumps({
-            'title': 'Prompt arguments', 
-            'type': 'object', 
-            'required' : list(dico.keys()),
-            'properties': dico
-        })
-
-    db.add(prompt_template)
-    db.commit()
-    
-    return {
-        "prompt" : {
-            "name" : prompt_template.name,
-            "template": prompt_template.template
-        },
-        "rjsf_ui" : {
-            'title': 'Prompt arguments', 
-            'type': 'object', 
-            'required': list(dico.keys()),
-            'properties': dico
-        }
+    prompt_template = {
+        "name": request.name,
+        "template": request.template,
+        "rjsf_ui": json.dumps(
+            {
+                "title": "Prompt arguments",
+                "type": "object",
+                "required": list(dico.keys()),
+                "properties": dico,
+            }
+        ),
+        "owner_id": user_id,
     }
 
+    store[request.name] = prompt_template
+
+    return {
+        "prompt": {
+            "name": prompt_template["name"],
+            "template": prompt_template["template"],
+        },
+        "rjsf_ui": {
+            "title": "Prompt arguments",
+            "type": "object",
+            "required": list(dico.keys()),
+            "properties": dico,
+        },
+    }
+
+
 @app.delete("/prompt/editor")
-def delete_prompt_template(keys:list[str], token:str=Depends(auth_layer), db:Session=Depends(get_db)) :
+def delete_prompt_template(
+    keys: list[str], user_id=Depends(get_auth_id_layer), db: Session = Depends(get_db)
+):
+    print(f"User {user_id} wants to save {keys}")
 
-    print(f"User {token} wants to delete {keys}")
+    store = SqlDictStore(
+        engine=engine,
+        table_name="prompt_template",
+        key_columns="name",
+        value_columns=["owner_id"],
+    )
 
-    for key in keys :
-        db.execute(delete(PromptTemplate).where(PromptTemplate.name == key))
-        print(f"Deleted {key}")
-    db.commit()
+    for key in keys:
+        if key in store:
+            if store[key]["owner_id"] == user_id:
+                store.__delitem__(key)
+
     return Response(status_code=200)
 
-class AskPromptRequest(BaseModel) :
-    template:str
-    parameters:list[str]
 
-def parse_parameters(template:str) -> list[str] :
-    pattern = r'\{([^}]*)\}'
+class AskPromptRequest(BaseModel):
+    template: str
+    parameters: list[str]
+
+
+def parse_parameters(template: str) -> list[str]:
+    pattern = r"\{([^}]*)\}"
     return re.findall(pattern, template)
 
-import oa
-from oa import prompt_function
-from i2 import Sig
 
 def _default_chat():
-    from oa import chat
-
     return chat
+
 
 def prompt_execution_adapter(prompt_template, params):
     f = prompt_function(prompt_template)
@@ -205,11 +225,11 @@ def prompt_execution_adapter(prompt_template, params):
     kwargs = sig.map_arguments(params, ignore_kind=True)
     return f(**kwargs)
 
+
 @app.post("/prompt/editor/ask")
-def ask_prompt(request:AskPromptRequest, token:str=Depends(auth_layer)) :
-    return {
-        "answer": prompt_execution_adapter(request.template, request.parameters)
-    }
+def ask_prompt(request: AskPromptRequest):
+    return {"answer": prompt_execution_adapter(request.template, request.parameters)}
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, log_level="trace")
