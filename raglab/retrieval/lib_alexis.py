a6625948fb8b0cce2241cf3eaf4fc4bfc30f6827
davidaleprog <alexisdavid1402@gmail.com>
2024-04-24 14:32:43 +0200

    added chunker function and query processing

--- raglab/retrieval/lib_alexis.py
+++ raglab/retrieval/lib_alexis.py
@@ -50,25 +50,38 @@ def query_embedding(query: str) -> np.ndarray:
     return np.array(embeddings_model.embed_query(query))
 
 
+_generate_split_keys = partial(generate_split_keys, chunk_size=30)
+
+
+# TODO : @cache_result : cache the result of this function
 def segment_keys(
-    documents: Mapping[DocKey, str], chunk_size: int = 30
+    documents: Mapping[DocKey, str],
+    chunker: Callable[
+        [Mapping[DocKey, str]], List[Tuple[str, int, int]]
+    ] = _generate_split_keys,
 ) -> List[Tuple[str, int, int]]:
-    return generate_split_keys(documents, chunk_size)
+    return chunker(documents)
 
 
 def doc_embeddings(
-    documents: Mapping[DocKey, str], segment_keys: List[Tuple[str, int, int]]
+    documents: Mapping[DocKey, str],
+    segment_keys: List[Tuple[str, int, int]],
+    embedding_function: Callable[
+        [List[str]], List[np.ndarray]
+    ] = embeddings_model.embed_documents,
 ) -> Mapping[Tuple[str, int, int], np.ndarray]:
     return dict(
         zip(
             segment_keys,
-            np.array(
-                embeddings_model.embed_documents(
-                    [documents[doc_key] for doc_key in documents]
-                )
-            ),
+            np.array(embedding_function([documents[doc_key] for doc_key in documents])),
         )
     )
+
+
+def dump_embeddings(
+    doc_embeddings: Mapping[Tuple[str, int, int], np.ndarray], path: str
+):
+    pass
 
 
 def top_k_segments(
@@ -92,7 +105,41 @@ def top_k_segments(
     return [key for _, key in top_k]
 
 
-funcs = [segment_keys, doc_embeddings, top_k_segments, query_embedding]
+import oa
+from functools import partial
+import tiktoken
+
+
+def query(user_query: str, process_function: Callable[str, str] = lambda x: x) -> str:
+    return process_function(user_query)
+
+
+def query_answer(
+    query: str,
+    documents: Mapping[DocKey, str],
+    top_k_segments: List[Tuple[str, int, int]],
+    prompt_template: str = "Answer question {query} using the following documents: {documents}",
+    llm_model: str = "gpt-3.5-turbo",
+) -> str:
+    chat_model = partial(oa.chat, model=llm_model)
+    aggregated_text = ""
+    for segment_key in top_k_segments:
+        aggregated_text += documents[segment_key[0]][segment_key[1] : segment_key[2]]
+    return prompt_function(prompt_template, prompt_func=chat_model)(
+        query=query, documents=aggregated_text
+    )
+
+
+from meshed import DAG
+
+funcs = [
+    segment_keys,
+    doc_embeddings,
+    top_k_segments,
+    query_embedding,
+    query_answer,
+    query,
+]
 dag = DAG(funcs)
 
 dag.dot_digraph()
