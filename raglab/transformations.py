d4bf069bc7751e6eb03b3a66c4e13024ea4123ae
thomasnappee <thomas.napro@gmail.com>
2024-05-13 12:51:50 +0000

    add utils for speech recognition

--- raglab/transformations.py
+++ raglab/transformations.py
@@ -13,18 +13,8 @@ def extract_text_from_pdf(pdf_path: str, lang='fra') -> str:
 
     return full_text
 
-
-
-def remove_silence_from_audio(audio_file_path, silence_threshold=-50.0, chunk_size=10):
-    audio = AudioSegment.from_file(audio_file_path)
-
-    non_silent_parts = silence.detect_nonsilent(
-        audio, min_silence_len=chunk_size, silence_thresh=silence_threshold
-    )
-
-    processed_audio = AudioSegment.silent(duration=0)
-
-    for start, end in non_silent_parts:
-        processed_audio += audio[start:end]
-
-    return processed_audio
+def convert_voice_to_text(voice_file, model="openai/whisper-medium"):
+    whisper = pipeline("automatic-speech-recognition", model=model)
+    text = whisper(voice_file.read())
+    del whisper
+    return text
